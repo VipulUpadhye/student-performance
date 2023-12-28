0518ad2287f4c0603e7c46d574b62337346a7fc4
Vipul Upadhye <vipulupadhye@gmail.com>
2023-12-28 16:52:34 -0500

    custom logger

---
+++ src/logger.py
@@ -0,0 +1,15 @@
+import logging
+from pathlib import Path
+from datetime import datetime
+
+LOG_FILE = f'{datetime.now().strftime("%m_%d_%Y_%H_%M_%S")}.log'
+logs_path = Path.cwd().joinpath(f'logs/{LOG_FILE}')
+logs_path.mkdir(exist_ok=True, parents=True)
+
+LOG_FILEPATH = logs_path.joinpath(LOG_FILE)
+
+logging.basicConfig(
+    filename=LOG_FILEPATH,
+    format='[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO, 
+)
