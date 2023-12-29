595e080f3bc8f6d1a04c189e59ccfd0313ffbc58
Vipul Upadhye <vipulupadhye@gmail.com>
2023-12-29 10:05:29 -0500

    create only parent folder logs

--- src/logger.py
+++ src/logger.py
@@ -3,7 +3,7 @@ from pathlib import Path
 from datetime import datetime
 
 LOG_FILE = f'{datetime.now().strftime("%m_%d_%Y_%H_%M_%S")}.log'
-logs_path = Path.cwd().joinpath(f'logs/{LOG_FILE}')
+logs_path = Path.cwd().joinpath('logs')
 logs_path.mkdir(exist_ok=True, parents=True)
 
 LOG_FILEPATH = logs_path.joinpath(LOG_FILE)
