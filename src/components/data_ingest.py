d9e551d9764e8a6c36230a813bab69462423ed79
Vipul Upadhye <vipulupadhye@gmail.com>
2023-12-30 10:48:59 -0500

    added comments for functions and steps

--- src/components/data_ingest.py
+++ src/components/data_ingest.py
@@ -29,18 +29,27 @@ class DataIngestion:
 
     def init_data_ingest(self):
         '''
-        Ingest and read data from input source.
+        Function for performing the data ingestion task.
+
+        Steps:
+        1. Ingest and read data from input source.
+        2. Split input data into train and test sets.
+        3. Save the train and test data to CSV.
         '''
         logging.info('Started the data ingestion component')
         try:
+            # Read the input data
             input_data_folder = Path(__file__).parent.parent.parent.joinpath('notebooks/data')
             df = pd.read_csv(input_data_folder.joinpath('stud.csv'))
             logging.info('Read the raw data in dataframe')
 
+            # Generate the "artifacts" folder for storing the output data files
             Path(self.ingestion_config.train_data_path).parent.mkdir(exist_ok=True, parents=True)
 
+            # Save the raw data to CSV
             df.to_csv(self.ingestion_config.raw_data_path, index=False, header=True)
 
+            # Split raw data into train and test sets, and save the datasets to CSV
             logging.info('Train-Test split started')
             train_set, test_set = train_test_split(df, test_size=0.2, random_state=10)
             train_set.to_csv(self.ingestion_config.train_data_path, index=False, header=True)
@@ -54,8 +63,3 @@ class DataIngestion:
         except Exception as err:
             logging.info('Error in data ingestion task!!')
             raise CustomException(err, sys)
-        
-
-if __name__ == '__main__':
-    obj = DataIngestion()
-    obj.init_data_ingest()
